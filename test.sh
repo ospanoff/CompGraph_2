1772aa3c927abed94ef172e14b7e433c8e082391
Ayat Ospanov <ayat.ospanov@gmail.com>
2014-10-23 23:35:52 +0400

    Added readme. Some fixes

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd build/bin
-./task2 -d ../../data/binary/test_labels.txt -m model.txt -l predictions.txt --predict
+./task2 -d ../../data/binary/test_labels.txt -m ../../model_binary.txt -l predictions.txt --predict
 cd ../..
--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd build/bin
-./task2 -d ../../data/binary/train_labels.txt -m model.txt --train
+./task2 -d ../../data/binary/train_labels.txt -m ../../model_binary.txt --train
 cd ../..
